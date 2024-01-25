97d828d22264490d4ad5365c44609c37909f6bc9
Vara-Kumar <142789755+Vara-Kumar@users.noreply.github.com>
2024-01-26 00:14:14 +0530

    Add files via upload

---
+++ src/main/java/com/employee/EMS/App.java
@@ -0,0 +1,13 @@
+package com.employee.EMS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
